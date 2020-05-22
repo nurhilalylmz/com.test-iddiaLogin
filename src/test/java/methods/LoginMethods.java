ccb0ed86d44e06ea99ac258b294e937f581d79b7
Hilal <nurhilalylmz@gmail.com>
2020-05-23 01:12:50 +0300

    Login properties düzenlendi. Türkçe karakter sorunu çözüldü.

--- src/test/java/methods/LoginMethods.java
+++ src/test/java/methods/LoginMethods.java
@@ -19,8 +19,11 @@ public class LoginMethods extends BaseMethods
     public LoginMethods login(String phoneNumber, String password) {
         waitForPageLoad(loginPage.copyrightText);
         writeText(loginPage.phoneNumber, phoneNumber);
+        waitSeconds(1);
         writeText(loginPage.password, password);
+        waitSeconds(1);
         clickElement(loginPage.buttonLogin);
+        waitSeconds(1);
         return new LoginMethods(driver);
     }
 
--- src/test/java/utils/ReadProperties.java
+++ src/test/java/utils/ReadProperties.java
@@ -1,6 +1,12 @@
 package utils;
 
+import org.openqa.selenium.interactions.Encodable;
+
+import javax.sound.sampled.AudioFormat;
+import java.beans.Encoder;
 import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.Properties;
 
 public class ReadProperties {
@@ -11,8 +17,8 @@ public class ReadProperties {
         FileInputStream fileInput = null;
         try {
             prop = new Properties();
-            fileInput = new FileInputStream(file);
-            prop.load(fileInput);
+            fileInput = (new FileInputStream(file));
+            prop.load(new InputStreamReader(fileInput, Charset.forName("ISO-8859-1")));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
